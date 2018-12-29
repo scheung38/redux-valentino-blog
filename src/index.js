830a88dc1d1876a7a7a3926466f7f4eb58943dfc
scheung38 <sebastian_cheung@yahoo.com>
2018-12-29 01:05:12 +0000

    middleware to block bad words

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Only for testing store. methods
 import index from "./js/index";
 
 
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,7 +1,12 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import rootReducer from "../reducers/index";
+import {forbiddenWordsMiddleware} from "../middleware";
 
+const storeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // Our central store to maintain external state of react components
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    storeEnchancers(applyMiddleware(forbiddenWordsMiddleware))
+);
 
 export default store;
